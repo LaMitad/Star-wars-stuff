ad1cf44e23d1955e288050cbbe2b46cd7e56a467
LaMitad <benjamin.moitie@gmail.com>
2017-08-22 14:04:12 +0200

    Add files via upload

---
+++ play.php
@@ -0,0 +1,33 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+
+require __DIR__.'/vendor/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
+$request = Request::createFromGlobals();
+//$response = $kernel->handle($request);
+//$response->send();
+//$kernel->terminate($request, $response);
+
+$container = $kernel->getContainer();
+$container->enterScope('request');
+$container->set('request', $request);
+
+use Yoda\EventBundle\Entity\Event;
+
+$event = new Event();
+$event->setName('Darth\'s surprise birthday party!');
+$event->setLocation('Deathstar');
+$event->setTime(new \datetime('tomorrow noon'));
+$event->setDetails('Ha! Darth HATES surprises!!!!!');
+
+$em = $container->get('doctrine')->getManager();
+$em->persist($event);
+$em->flush();
